0f38c3de88479ca1f8883a71df4794cdbde3dc5c
Maitri Mydur <mmydur@gmail.com>
2025-06-15 15:36:42 -0700

    Remove duplicate Header from Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+import Header from './components/Header'
 import Home from './pages/Home'
+import Research from './pages/Research'
+import CodePage from './pages/Code'
+import AppPage from './pages/AppPage'
+
 import './App.css'
 
 export default function App() {
-  return <Home />
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/research" element={<Research />} />
+        <Route path="/code"    element={<CodePage />} />
+        <Route path="/app"     element={<AppPage />} />
+      </Routes>
+    </>
+  )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from '../components/Header'
+// import Header from '../components/Header'    ← remove this
 import Hero from '../components/Hero'
 import Features from '../components/Features'
 import WhyItMatters from '../components/WhyItMatters'
@@ -9,7 +9,7 @@ import Footer from '../components/Footer'
 export default function Home() {
   return (
     <div className="App">
-      <Header />
+      {/* <Header />  ← remove this */}
       <main>
         <Hero />
         <Features />
